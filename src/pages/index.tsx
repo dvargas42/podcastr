1427ca9f75b78ee6dd04d582e67ad592e51d0d39
dvargas42 <dvargas.eng@gmail.com>
2021-04-21 01:34:36 -0300

    staticprops function implamentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { GetServerSideProps } from 'next'
+
+export default function Home(props) {
   return (
-    <h1>Hello World!</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, //8 hours
+  }
 }
