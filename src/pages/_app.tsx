d458536dc71d134b68a70290af05714bbd7b8fd6
dvargas42 <dvargas.eng@gmail.com>
2021-04-21 01:33:46 -0300

    importation of the player component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import styles from '../styles/app.module.scss'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
   )
   
 }
