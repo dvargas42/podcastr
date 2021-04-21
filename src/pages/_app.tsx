c3474eb19ccfa0d0fd7e4760bcd61130624b500a
dvargas42 <dvargas.eng@gmail.com>
2021-04-20 23:48:18 -0300

    files rename

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../components/Header'
+import '../styles/global.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
+  
+}
+
+export default MyApp
