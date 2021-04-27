08924b05f408efed63fbd5c910bb102164b87d24
dvargas42 <dvargas.eng@gmail.com>
2021-04-27 20:57:48 -0300

    dark mode implementation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from 'react';
 import format from 'date-fns/format'
 import ptBR from 'date-fns/locale/pt-BR'
 
 import styles from './styles.module.scss'
 
 export function Header() {
+  let page
+
+  useEffect(() => {
+    page = document.querySelector('html')
+    page.dataset.theme = `theme-dark`
+  }, [])
+
+  function toggleDarkMode() {
+    if (page.dataset.theme === 'theme-dark') {
+      page.dataset.theme = `theme-light`;
+    } else {
+      page.dataset.theme = `theme-dark`;
+    }
+
+  }
+
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
     locale: ptBR,
   })
+
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr"/>
+      <img src="/logo.svg" alt="Podcastr" />
 
       <p>O melhor para você ouvir, sempre</p>
 
+      <button onClick={toggleDarkMode}>
+        <img src="/sun-and-moon.svg" alt="" />
+      </button>
       <span>{currentDate}</span>
     </header>
   )
